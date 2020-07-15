139e6b7bb339b911e7a3abbb4a45c6de0107ddeb
Nishant Sahay <37246425+NishantSahay7@users.noreply.github.com>
2020-07-15 22:38:37 +0530

    Added Comments

--- sketch_jul09a/sketch_jul09a.ino
+++ sketch_jul09a/sketch_jul09a.ino
@@ -3,7 +3,7 @@
 
 /**********Google Sheets Definations***********/
 char column_name_in_sheets[ ][20] = {"value1","value2","value3"};                        /*1. The Total no of column depends on how many value you have created in Script of Sheets;2. It has to be in order as per the rows decided in google sheets*/
-String Sheets_GAS_ID = "Enter your GAS ID Here";       /*This is the Sheets GAS ID, you need to look for your sheets id*/
+String Sheets_GAS_ID = "Enter your GAS ID Here";                                         /*This is the Sheets GAS ID, you need to look for your sheets id*/
 int No_of_Parameters = 3;                                                                /*Here No_of_Parameters decides how many parameters you want to send it to Google Sheets at once, change it according to your needs*/
 /*********************************************/
 
@@ -12,8 +12,8 @@ void setup()
   Serial.begin(9600);
   while (!Serial);
 
-  WIFI_Connect("SSID","Password");                                          /*Provide you Wi-Fi SSID and password to connect to Wi-Fi*/
-  Google_Sheets_Init(column_name_in_sheets, Sheets_GAS_ID, No_of_Parameters );           /*Sets the column name for Google Sheets, the GAS ID, and the No of Parameter we want to send*/        
+  WIFI_Connect("SSID","Password");                                                     /*Provide you Wi-Fi SSID and password to connect to Wi-Fi*/
+  Google_Sheets_Init(column_name_in_sheets, Sheets_GAS_ID, No_of_Parameters );         /*Sets the column name for Google Sheets, the GAS ID, and the No of Parameter we want to send*/        
 }
 
 void loop() 
@@ -23,5 +23,6 @@ void loop()
   Data_to_Sheets(No_of_Parameters,  a,  b, c);         /*1. This function accepts multiple float parameter, here No_of_Parameters decides how many parameters you want to send to Google Sheets; 2. The values sent should be in order as per the column in Google Sheets*/
 
   Serial.println();
-  delay(10000);       /*10 Sec Delay*/
+  delay(10000);                                       /*10 Sec Delay, Here 10 second delay is just used so that we can see the data pushed to sheets one by one
+                                                        There is nothing like a minimum delay between two data push*/
 }
