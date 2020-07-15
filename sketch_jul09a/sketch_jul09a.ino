2044f24e88e230340a45207c172132eaad41aab8
Nishant Sahay <37246425+NishantSahay7@users.noreply.github.com>
2020-07-15 22:33:07 +0530

    Update sketch_jul09a.ino

--- sketch_jul09a/sketch_jul09a.ino
+++ sketch_jul09a/sketch_jul09a.ino
@@ -3,7 +3,7 @@
 
 /**********Google Sheets Definations***********/
 char column_name_in_sheets[ ][20] = {"value1","value2","value3"};                        /*1. The Total no of column depends on how many value you have created in Script of Sheets;2. It has to be in order as per the rows decided in google sheets*/
-String Sheets_GAS_ID = "AKfycbwJ89vo7DoaS5tJw5-vYvJpCgEcj4YS3nv7bkY4ASqgVRWqkxve";       /*This is the Sheets GAS ID, you need to look for your sheets id*/
+String Sheets_GAS_ID = "Enter your GAS ID Here";       /*This is the Sheets GAS ID, you need to look for your sheets id*/
 int No_of_Parameters = 3;                                                                /*Here No_of_Parameters decides how many parameters you want to send it to Google Sheets at once, change it according to your needs*/
 /*********************************************/
 
@@ -12,7 +12,7 @@ void setup()
   Serial.begin(9600);
   while (!Serial);
 
-  WIFI_Connect("X-men trainee","Qwertyuiop69");                                          /*Provide you Wi-Fi SSID and password to connect to Wi-Fi*/
+  WIFI_Connect("SSID","Password");                                          /*Provide you Wi-Fi SSID and password to connect to Wi-Fi*/
   Google_Sheets_Init(column_name_in_sheets, Sheets_GAS_ID, No_of_Parameters );           /*Sets the column name for Google Sheets, the GAS ID, and the No of Parameter we want to send*/        
 }
 
